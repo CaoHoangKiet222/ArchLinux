21cb20c6818b65defc0b61d6ff875ad2c34d99fc
CaoHoangKiet222 <caohoangkiet1720@gmail.com>
2022-05-31 11:44:08 +0700

    Add settings for folding

--- nvim/lua/dogboy/settings.lua
+++ nvim/lua/dogboy/settings.lua
@@ -33,6 +33,7 @@ local options = {
 	scrolloff = 8, -- is one of my fav
 	sidescrolloff = 8,
 	guifont = "Fira Code:h17", -- the font used in graphical neovim applications
+	foldmethod = "indent",
 }
 
 vim.opt.shortmess:append("c")
