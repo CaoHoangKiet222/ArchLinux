d5d4d28e8da0fb94dd43ea459958449bd66cc6ea
CaoHoangKiet222 <caohoangkiet1720@gmail.com>
2022-07-08 06:09:59 +0700

    Add i3 changes

--- i3/togglePicom/toggle_picom.sh
+++ i3/togglePicom/toggle_picom.sh
@@ -5,4 +5,9 @@ do
   sleep 1;
 done
 
+polybar=$(pgrep -x polybar > /dev/null && echo "Process found" || echo "Process not found")
+if [[ $polybar == "Process not found" ]]; then
+   ~/.config/polybar/launch.sh &	
+fi
+
 picom --experimental-backends
