2ea66b9de54a70a08a25921c5630b2fbfcebfbdc
CaoHoangKiet222 <caohoangkiet1720@gmail.com>
2023-02-24 13:46:25 +0700

    [modify]: modify i3

--- i3/changeLanguage/change_language.sh
+++ i3/changeLanguage/change_language.sh
@@ -16,11 +16,13 @@ fi
 if [ $lang = $EN_ibus ]
 then
   ibus engine $VN_ibus
-  notify-send "Ibus" "Vietnamese" --icon="$PICDIR/flagVietNam.png" -t 1000 -a "System"
+  killall dunst
+  dunstify "Ibus" "Vietnamese" --icon="$PICDIR/flagVietNam.png" -t 1000 -a "System"
 fi
 
 if [ $lang = $VN_ibus ]
 then
   ibus engine $EN_ibus
-  notify-send "Ibus" "English" --icon="$PICDIR/flagUnitedStates.jpg" -t 1000 -a "System"
+  killall dunst
+  dunstify "Ibus" "English" --icon="$PICDIR/flagUnitedStates.jpg" -t 1000 -a "System"
 fi
--- i3/toggleCapsLock/toggle_caps.sh
+++ i3/toggleCapsLock/toggle_caps.sh
@@ -3,9 +3,11 @@ PICDIR=$HOME/.config/Pictures/Wallpapers-Caplock
 
 if [ $status == "off" ]
 then
-  notify-send "Caps Lock" "On" --icon="$PICDIR/caps-lock-on.png" -t 1000 -a "System"
+  killall dunst
+  dunstify "Caps Lock" "On" --icon="$PICDIR/caps-lock-on.png" -t 1000 -a "System"
 else
-  notify-send "Caps Lock" "Off" --icon="$PICDIR/caps-lock-off.png" -t 1000 -a "System"
+  killall dunst
+  dunstify "Caps Lock" "Off" --icon="$PICDIR/caps-lock-off.png" -t 1000 -a "System"
 fi
 
 xdotool key Caps_Lock
