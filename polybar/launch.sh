febd541ca720e3c24d8a2b1e2287edfdace9c70d
CaoHoangKiet222 <caohoangkiet1720@gmail.com>
2023-03-07 20:29:48 +0700

    [feat]: new polybar for HDMI monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,13 +2,32 @@
 
 # Terminate already running bar instances
 killall -q polybar
+time=0.15
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar topbar-right -r -c ~/.config/polybar/config.ini &
-polybar topbar-dogboy -r -c ~/.config/polybar/config.ini &
-polybar topbar-center -r -c ~/.config/polybar/config.ini &
-polybar topbar-left -r -c ~/.config/polybar/config.ini &
-polybar topbar-tray -r -c ~/.config/polybar/config.ini &
+sleep $time
+polybar topbar-right -r -c ~/.config/polybar/Main/config.ini &
+sleep $time
+polybar topbar-dogboy -r -c ~/.config/polybar/Main/config.ini &
+sleep $time
+polybar topbar-center -r -c ~/.config/polybar/Main/config.ini &
+sleep $time
+polybar topbar-left -r -c ~/.config/polybar/Main/config.ini &
+sleep $time
+polybar topbar-tray -r -c ~/.config/polybar/Main/config.ini &
+
+if [[ $(xrandr -q | grep "HDMI-1 connected") ]]; then
+  sleep $time
+  polybar topbar-left -r -c ~/.config/polybar/Monitor/config.ini &
+  sleep $time
+  polybar topbar-tray -r -c ~/.config/polybar/Monitor/config.ini &
+  sleep $time
+  polybar topbar-center -r -c ~/.config/polybar/Monitor/config.ini &
+  sleep $time
+  polybar topbar-right -r -c ~/.config/polybar/Monitor/config.ini &
+  sleep $time
+  polybar topbar-dogboy -r -c ~/.config/polybar/Monitor/config.ini &
+fi
